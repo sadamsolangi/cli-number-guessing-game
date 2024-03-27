7df8cb48a34906810dad4abe39ee4575e754a9c1
Sadam Solangi <143290583+sadamsolangi@users.noreply.github.com>
2024-03-27 04:46:22 -0700

    pushing cli number guessing game

---
+++ main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+//wellcome user 
+console.log("Wellcome to the number guessing game");
+const answers = await inquirer.prompt([{
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1-10"
+    }
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("Congratulations you guessed a number");
+}
+else {
+    console.log("oh no you not guessed a number next time better luck!");
+}
